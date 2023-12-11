bcb2b4f0dc1191a89251afa24ff6e564b4d8f480
Chad Wallace <cewall0@gmail.com>
2023-12-11 08:54:54 -0500

    Initial Commit

---
+++ RememberTrump/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RememberTrump
+//
+//  Created by Chad Wallace on 12/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RememberTrump/RememberTrumpApp.swift
@@ -0,0 +1,17 @@
+//
+//  RememberTrumpApp.swift
+//  RememberTrump
+//
+//  Created by Chad Wallace on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct RememberTrumpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
